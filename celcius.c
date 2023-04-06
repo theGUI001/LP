7c09b37f9977a55fe353fd91f0304cfb82fc57be
theGUI001 <lghmelo@gmail.com>
2023-04-06 10:18:03 -0300

    LP first ex done

---
+++ celcius.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, f;
+
+    printf("Digite a temperatura, em graus celcius, que deseja converter para fahrenheit");
+    scanf("%i", &c);
+
+    f = (c * 9/5) + 32;
+
+    printf("%i graus celcius em fahrenheit é: %i\n", c, f);
+    return 0;
+}
