605302ac91f6219a50ee74fca2200becd46ac38e
theGUI001 <lghmelo@gmail.com>
2023-04-06 10:18:11 -0300

    LP 2nd ex done

---
+++ fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    double t2 = 0, t1 = 1, t;
+    int n, counter = 0;
+
+    printf("Digite o número da sequencia de fibonacci: ");
+    scanf("%d", &d);
+
+    while (counter = < n)
+    {
+        t = t2 + t1;
+        t2 = t1;
+        t1 = t;
+        counter++;
+    }
+
+    printf("A o número %i da sequência de fibonacci é: %d", n, t)
+}
