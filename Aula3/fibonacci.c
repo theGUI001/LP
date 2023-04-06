2ee56863912e3a99ae04b76c27893e07c6101ba8
theGUI001 <lghmelo@gmail.com>
2023-04-06 16:52:20 -0300

    fibonnaci working

--- Aula3/fibonacci.c
+++ Aula3/fibonacci.c
@@ -4,12 +4,24 @@ int main()
 {
 
     double t2 = 0, t1 = 1, t;
-    int n, counter = 0;
+    int n, counter = 2;
 
     printf("Digite o número da sequencia de fibonacci: ");
-    scanf("%d", &d);
+    scanf("%i", &n);
 
-    while (counter = < n)
+    if (n == 1)
+    {
+        printf("A o número %i da sequência de fibonacci é: %.0lf\n", n, t2);
+        return 0;
+    }
+
+    if (n == 2)
+    {
+        printf("A o número %i da sequência de fibonacci é: %.0lf\n", n, t1);
+        return 0;
+    }
+
+    while (counter < n)
     {
         t = t2 + t1;
         t2 = t1;
@@ -17,5 +29,6 @@ int main()
         counter++;
     }
 
-    printf("A o número %i da sequência de fibonacci é: %d", n, t)
+    printf("A o número %i da sequência de fibonacci é: %.0lf\n", n, t);
+    return 0;
 }
