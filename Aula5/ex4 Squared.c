08a354df8dc2f2e49e3d1e0f771e4462fe2cdb7b
theGUI001 <lghmelo@gmail.com>
2023-04-15 01:45:56 -0300

    ex4 (aula 5) done

---
+++ Aula5/ex4 Squared.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i;
+
+    printf("Digite um número pra elevar ao quadrado: ");
+    scanf("%i", &i);
+
+    printf("%i ao quadrado é: %i\n", i, i * i);
+    return 0;
+}
