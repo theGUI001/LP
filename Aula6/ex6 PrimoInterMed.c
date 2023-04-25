87a75e2ebda2d255a9a349a8af977d3a37e5fe29
theGUI001 <lghmelo@gmail.com>
2023-04-25 10:43:24 -0300

    ex6 Aula6 done

---
+++ Aula6/ex6 PrimoInterMed.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int st = 0, end = 0, med = 0, count = 0;
+    short flag;
+
+    printf("Digite o valor inicial do intervalo (inclusivo): ");
+    scanf("%i", &st);
+
+    printf("Digite o valor final do intervalo (inclusivo): ");
+    scanf("%i", &end);
+
+    if (end < st)
+    {
+        printf("O valor final do intervalor é maior que o inicial. Tente novamente com outros valores\n");
+    }
+
+    printf("\nOs números primos no intervalo de %i a %i são: ", st, end);
+
+    if (st < 2)
+    {
+        st = 2;
+    }
+
+    for (st; st <= end; st++)
+    {
+        flag = 0;
+
+        for (int i = 2; i <= (st / 2); i++)
+        {
+            if ((st % i) == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            printf("%i ", st);
+            med += st;
+            count += 1;
+        }
+    }
+    printf("\nA média desses números primos é: %.2f\n\n", ((float)med / count));
+}
