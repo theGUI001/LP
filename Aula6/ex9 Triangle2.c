c3cc202a81d7518a8044f460a047f41793804a74
theGUI001 <lghmelo@gmail.com>
2023-04-25 10:48:29 -0300

    ex9 Aula6 done

---
+++ Aula6/ex9 Triangle2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, b, c, swap;
+
+    printf("Digite os lados dos triângulos os separando por espaços: ");
+    scanf("%i %i %i", &a, &b, &c);
+
+    // Sort sides in ASC order
+    if (a > b)
+    {
+        swap = a;
+        a = b;
+        b = swap;
+    }
+
+    if (b > c)
+    {
+        swap = b;
+        b = c;
+        c = swap;
+    }
+
+    if (a > b)
+    {
+        swap = a;
+        a = b;
+        b = swap;
+    }
+
+    // Verifies if is a triangle
+    if (a + b <= c)
+    {
+        printf("Não é um triângulo válido.\n");
+        return 1;
+    }
+
+    // Verifies what "kind" of triangle it's
+    if (pow(c, 2) == pow(a, 2) + pow(b, 2))
+    {
+        printf("Triângulo Retângulo\n");
+        return 0;
+    }
+
+    if (pow(c, 2) > pow(a, 2) + pow(b, 2))
+    {
+        printf("Triângulo Obtusângulo\n");
+        return 0;
+    }
+
+    printf("Triângulo Acutângulo\n");
+    return 0;
+}
