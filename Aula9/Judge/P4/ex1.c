cba6b13abff6416180ce1c46d56a579cd8ad02bc
theGUI001 <lghmelo@gmail.com>
2023-05-10 16:56:20 -0300

    ex1 P4 Aula 9 done

---
+++ Aula9/Judge/P4/ex1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int tmp = 0, intcount = 0, prod = 1;
+    short x = 1;
+    float mult7 = 0, countMult7 = 0;
+
+    while (x != 0)
+    {
+        scanf("%i", &tmp);
+        if (tmp > -1)
+        {
+            intcount++;
+            if (tmp % 7 == 0)
+            {
+                mult7 += tmp;
+                countMult7++;
+            }
+            if (tmp % 15 == 0)
+            {
+                prod *= tmp;
+            }
+        }
+        else if (tmp == -1)
+        {
+            x = 0;
+        }
+    }
+
+    if (countMult7 <= 0)
+    {
+        mult7 = 0.0;
+    }
+    mult7 /= countMult7;
+
+    printf("%i %f %i\n", intcount, mult7, prod);
+}
