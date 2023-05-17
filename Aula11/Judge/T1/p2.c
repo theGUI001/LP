ed94a6dadfa663e9c3df240155cdd9fb73a8d1ad
theGUI001 <lghmelo@gmail.com>
2023-05-17 16:23:04 -0300

    p2, T1, Aula 11 done

---
+++ Aula11/Judge/T1/p2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+long long int myPow(int x, int p);
+
+int main()
+{
+
+    int V[5] = {0, 0, 0, 0, 0}, i = 0, p;
+
+    while (i < 5)
+    {
+        scanf("%i", &V[i]);
+        if (V[i] > 10 || V[i] < -10)
+        {
+            scanf("%i", &V[i]);
+        }
+        else
+            i++;
+    }
+
+    scanf("%i", &p);
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("%lld ", myPow(V[i], p));
+    }
+}
+
+long long int myPow(int x, int p)
+{
+    int resultado = 1;
+
+    for (int i = 1; i <= p; i++)
+    {
+        resultado *= x;
+    }
+
+    return resultado;
+}
