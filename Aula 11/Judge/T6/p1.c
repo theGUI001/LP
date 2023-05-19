e47a43c8fd2b600f186b9360dff4f5e59f283698
theGUI001 <lghmelo@gmail.com>
2023-05-19 13:57:35 -0300

    optimization

--- Aula 11/Judge/T6/p1.c
+++ Aula 11/Judge/T6/p1.c
@@ -4,31 +4,19 @@
 
 int main()
 {
-
     int i, j, M[MAX][MAX], F[101] = {0};
 
     scanf("%i %i", &i, &j);
 
     for (int w = 0; w < i; w++)
-    {
         for (int z = 0; z < j; z++)
         {
             scanf("%i", &M[w][z]);
-        }
-    }
 
-    for (int w = 0; w < i; w++)
-    {
-        for (int z = 0; z < j; z++)
-        {
             F[M[w][z]]++;
         }
-    }
+
     for (int r = 0; r <= 100; r++)
-    {
         if (F[r] > 0)
-        {
             printf("%i %i\n", r, F[r]);
-        }
-    }
 }
